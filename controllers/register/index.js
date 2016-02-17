5294a38fda4adafef2237c19fc90659c40393690
dennishall1 <dennis.hall@razorfish.com>
2016-02-17 16:20:06 -0500

    update comments

--- controllers/register/index.js
+++ controllers/register/index.js
@@ -10,31 +10,24 @@ module.exports = function (router) {
 
 
     /**
-     * Display the login page. We also want to display any error messages that result from a failed login attempt.
+     * Display the register page.
      */
     router.get('/', function (req, res) {
-
-        //Include any error messages that come from the login process.
-        model.messages = req.flash('error');
         res.render('register', model);
     });
 
     /**
-     * Receive the login credentials and authenticate.
+     * Receive the registration credentials.
      * Successful authentications will go to /profile or if the user was trying to access a secured resource, the URL
      * that was originally requested.
      *
      * Failed authentications will go back to the login page with a helpful error message to be displayed.
      */
     router.post('/', function (req, res) {
-
-        // todo - check if email is taken...
-        // todo - create a new route just for that, so it can be used via ajax to enhance the register form.
+        var newUserCandidate = new User(req.body);
 
         // thwart attempts to bypass email verification
-        req.body.isEmailVerified = false;
-
-        var newUserCandidate = new User(req.body);
+        newUserCandidate.set('isEmailVerified', false);
 
         //Retrieve the user from the database by login
         User.findOne({email: newUserCandidate.email}, function(err, user) {
@@ -44,22 +37,23 @@ module.exports = function (router) {
             } else {
                 if(user) {
                     // user with that email address already exists.
-                    delete user.password;
-                    res.render('register', {user: user, messages: ['Email already in use']});
+                    delete newUserCandidate.password;
+                    res.render('register', {user: newUserCandidate, messages: ['Email already in use']});
                 } else {
-                    newUserCandidate.save();
-                    newUserCandidate.sendVerificationEmail(req, function(err){
-                        if(err){
-                            res.end(err);
-                        } else {
-                            res.render('register-success', {user: newUserCandidate});
-                        }
+                    newUserCandidate.save(function(err){
+                        // assume that saving the user went ok
+                        newUserCandidate.sendVerificationEmail(req, function(err){
+                            if(err){
+                                res.end(err);
+                            } else {
+                                res.render('register-success', {user: newUserCandidate});
+                            }
+                        });
                     });
                 }
             }
         });
 
     });
-
 
 };
